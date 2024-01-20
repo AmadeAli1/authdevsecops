f2e90c6de103d498e55259561d366ea325d5896c
AmadeAli1 <aliamade29@gmail.com>
2024-01-20 14:33:37 +0200

    Initial Commit

--- src/test/kotlin/com/amade/dev/authdevsecops/AuthDevSecOpsApplicationTests.kt
+++ src/test/kotlin/com/amade/dev/authdevsecops/AuthDevSecOpsApplicationTests.kt
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class AuthDevSecOpsApplicationTests {
+class AuthDevSecOpsApplicationTests constructor() {
 
     @Test
     fun contextLoads() {
+        println("Say Hello contextLoads")
     }
 
 }
